79359aff89a3090011a85a56594f6e45f39d019d
rubenscn2011 <rubenscn2011@gmail.com>
2026-05-04 21:54:54 -0300

    Operadores em JS

---
+++ Operadores/desafioOperadores.js
@@ -0,0 +1,18 @@
+// DESAFIO
+
+/*
+ Atalho: SHIFT + ALT + A
+
+ 01. Utilizando os operadores relacionais, declare 2 variáveis e imprima no output/console 5 valores TRUE e 5 valores FALSE
+
+
+ */
+
+// Declare 2 variáveis
+let a = 14; 
+let b = 18;
+ 
+// Valores TRUE
+
+
+// Valores FALSE
---
+++ Operadores/unario.js
@@ -0,0 +1,11 @@
+let a = 1; // 1
+
+a = a + 1; // 2
+a =+ 1; // 3
+a++; // 4
+console.log(a);
+
+a = a - 1; // 1
+-= 1; // 2
+a--; // Unário // 1 Unário
+console.log(a);
